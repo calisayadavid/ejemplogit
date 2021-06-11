0e8a2b7b894f06e0624f9da3f48157851e19ee14
davidcalisaya <davicho123@gmail.com>
2021-06-11 14:51:10 -0300

    primera version

---
+++ src/ejemplogit/EjemploGit.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogit;
+
+/**
+ *
+ * @author david
+ */
+public class EjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a,b,c=0;
+        a=34;
+        b=123;
+        c=a+b;
+        System.out.println("la suma es:" + c);
+        
+    }
+    
+}
